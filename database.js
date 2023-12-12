6e51a6d3889dea27163c444726a47af8e63a7b10
Michael Bradshaw <michaelbradshaw500@gmail.com>
2023-12-11 17:25:07 -0700

    update dependency references

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const config = require('./public/dbConfig.json');
+const config = require('./dbConfig.json');
 
 // Connect to the database cluster
 const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
